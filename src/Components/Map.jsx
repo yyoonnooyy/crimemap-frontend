57b33020076818d873db0d3389e4fa9ffd021332
[yoonyoungS2] <[sss121@swu.ac.kr]>
2022-06-20 06:25:10 +0900

    Update safepath files

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./map.css";
 import $ from "jquery";
@@ -6,6 +6,52 @@ import $ from "jquery";
 const { Tmapv2 } = window;
 
 const Map = () => {
+
+  var new_polyLine = [];
+  var new_Click_polyLine = [];
+
+  function drawData(data, map) {
+    // 지도위에 선은 다 지우기
+    var resultStr = "";
+    var distance = 0;
+    var idx = 1;
+    var newData = [];
+    var equalData = [];
+    var pointId1 = "-1234567";
+    var ar_line = [];
+    var pointArray = [];
+    
+    for (var i = 0; i < data.features.length; i++) {
+      var feature = data.features[i];
+      //배열에 경로 좌표 저장
+      if(feature.geometry.type == "LineString"){
+        ar_line = [];
+        for (var j = 0; j < feature.geometry.coordinates.length; j++) {
+          var startPt = new Tmapv2.LatLng(feature.geometry.coordinates[j][1],feature.geometry.coordinates[j][0]);
+          ar_line.push(startPt);
+          pointArray.push(feature.geometry.coordinates[j]);
+        }
+        var polyline = new Tmapv2.Polyline({
+              path: ar_line,
+              strokeColor: "#ff0000", 
+              strokeWeight: 6,
+              map: map
+          });
+        new_polyLine.push(polyline);
+      }
+      var pointId2 = feature.properties.viaPointId;
+      if (pointId1 != pointId2) {
+        equalData = [];
+        equalData.push(feature);
+        newData.push(equalData);
+        pointId1 = pointId2;
+      }
+      else {
+        equalData.push(feature);
+      }
+    }
+  } // drawData();
+
   useEffect(
     () => {
       const map = new Tmapv2.Map("myMap", {
@@ -18,10 +64,9 @@ const Map = () => {
       var markerArr = [];
       var lonlat;
 
-      $("#startPoint").keydown(
-        function (key) {
-          if (key.keyCode === 13) {
-            var searchKeyword = $("#startPoint").val();
+      // 버튼
+      $("#btn_start").click(function (key) {
+        var searchKeyword = $("#startPoint").val();
             $.ajax({
               method: "GET",
               url: "https://apis.openapi.sk.com/tmap/pois?version=1&format=json&callback=result",
@@ -104,6 +149,7 @@ const Map = () => {
                       $("#searchResult").html("도착지를 설정하세요!");
                       markerArr.push(marker);
                     }
+
                   }
                 }
 
@@ -124,16 +170,15 @@ const Map = () => {
                 );
               }, //error
             }); //ajax
-          } //if
-        } //startPoint
-      ); //$("#strartPoint")
+        }
+      ); //$("#btn_start")
+
 
       // 도착지 설정 - POI 통합 검색 API 요청
       var markerArr = [];
 
-      $("#endPoint").keydown(
-        function (key) {
-          if (key.keyCode === 13) {
+      // 버튼
+      $("#btn_end").click(function (key) {
             var searchKeyword = $("#endPoint").val();
             $.ajax({
               method: "GET",
@@ -235,22 +280,16 @@ const Map = () => {
                 );
               }, //error
             }); //ajax
-          } //if
-        } //startPoint
-      );
+        } //endPoint
+      ); //$(#btn_end)
 
-      let marker_s, marker_e, marker;
+      let marker_s, marker_e, marker, marker2;
 
       //경로 선 그리기
       let headers = {};
       headers["appKey"] = "l7xxf4d6ce3985d1419b9a268be498b40d48";
 
       $("#btn_select").click(function (key) {
-        // 두 점 사이 직선 거리
-        var distance = marker_s
-          .getPosition()
-          .distanceTo(marker_e.getPosition());
-        console.log("거리: ", distance);
 
         //사각형
         var rect = new Tmapv2.Rectangle({
@@ -265,108 +304,122 @@ const Map = () => {
             )
           ),
           strokeColor: "red",
-          fillColor: "green",
+          fillColor: "yellow",
           map: map,
         });
 
-        //경유지
-        marker = new Tmapv2.Marker({
-          position: new Tmapv2.LatLng(37.56626352138058, 126.98735015742581),
-          icon: "http://tmapapi.sktelecom.com/upload/tmap/marker/pin_b_m_1.png",
-          iconSize: new Tmapv2.Size(24, 38),
-          map: map,
-        });
+        var bounds2 = new Tmapv2.LatLngBounds(
+            new Tmapv2.LatLng(
+              marker_s.getPosition()._lat,
+              marker_s.getPosition()._lng
+            ),
+            new Tmapv2.LatLng(
+              marker_e.getPosition()._lat,
+              marker_e.getPosition()._lng
+            )
+          )
 
-        //경로 그리기
-        let headers = {};
-        headers["appKey"] = "l7xxf4d6ce3985d1419b9a268be498b40d48";
-
+        var fileName = "cctv_2.csv";
+        var latitude = [];
+        var longitude= [];
         $.ajax({
-          type: "POST",
-          headers: headers,
-          url: "https://apis.openapi.sk.com/tmap/routes/routeOptimization20?version=1&format=json", //
-          async: false,
-          contentType: "application/json",
-          data: JSON.stringify({
-            reqCoordType: "WGS84GEO",
-            resCoordType: "EPSG3857",
-            startName: "출발",
-            startX: String(marker_s.getPosition()._lng),
-            startY: String(marker_s.getPosition()._lat),
-            startTime: "202111121314",
-            endName: "도착",
-            endX: String(marker_e.getPosition()._lng),
-            endY: String(marker_e.getPosition()._lat),
-            searchOption: "0",
-            viaPoints: [
-              {
-                viaPointId: "test01",
-                viaPointName: "test01",
-                viaX: "126.98735015742581",
-                viaY: "37.56626352138058",
-              },
-            ],
-          }),
-
-          success: function (response) {
-            var resultData = response.properties;
-            var resultFeatures = response.features;
-
-            console.log(resultFeatures);
-
-            for (var i in resultFeatures) {
-              var geometry = resultFeatures[i].geometry;
-              var properties = resultFeatures[i].properties;
-              var polyline_;
-
-              let drawInfoArr = [];
-
-              if (geometry.type === "LineString") {
-                for (var j in geometry.coordinates) {
-                  // 경로들의 결과값을 포인트 객체로 변환
-                  var latlng = new Tmapv2.Point(
-                    geometry.coordinates[j][0],
-                    geometry.coordinates[j][1]
-                  );
-                  var convertPoint =
-                    new Tmapv2.Projection.convertEPSG3857ToWGS84GEO(latlng);
-                  var convertChange = new Tmapv2.LatLng(
-                    convertPoint._lat,
-                    convertPoint._lng
-                  );
-
-                  drawInfoArr.push(convertChange);
-                }
-
-                polyline_ = new Tmapv2.Polyline({
-                  path: drawInfoArr,
-                  strokeColor: "#FF0000",
-                  strokeWeight: 6,
-                  map: map,
-                });
+          url: fileName,
+          dataType:'text',
+          success: function(data) {
+            for(var count = 0; count < data.length; count++) {
+                var collapse = data.split("\n");
+              }
+            alert(collapse.length);
+            /*var singleRow = [];
+            var result = [];
+            for(var count = 0; count < data.length; count++) {
+              var collapse = data.split("\n");
+            }
+            for(var count = 0; count < collapse.length; count++) {
+                singleRow = [];
+                singleRow = collapse[count].split("/");
+                latitude[count] = singleRow[2];
+                longitude[count] = singleRow[3];
+            }
+            var lat = [];
+            var lng = [];
+            var i = 0;
+            for(var count = 0; count < latitude.length; count++) {
+              var tired = new Tmapv2.LatLng(latitude[count],longitude[count]);
+              if(bounds2.contains(tired)) {
+                lat[i]= latitude[count];
+                lng[i]= longitude[count];
+                //i= i + 1;
+                break;
               }
             }
-          },
-          error: function (request, status, error) {
-            console.log(
-              "code:" +
-                request.status +
-                "\n" +
-                "message:" +
-                request.responseText +
-                "\n" +
-                "error:" +
-                error
-            );
-          },
-        }); //경로 그리기
+            
+            //경유지 설정
+            marker = new Tmapv2.Marker({
+              position: new Tmapv2.LatLng(lat[0],lng[0]),
+              icon: "http://tmapapi.sktelecom.com/upload/tmap/marker/pin_b_m_1.png",
+              iconSize: new Tmapv2.Size(24, 38),
+              map: map,
+            });
+
+            //경로 그리기
+            let headers = {};
+            headers["appKey"] = "l7xxf4d6ce3985d1419b9a268be498b40d48";
+            var passList = "127.0338631,37.51518948";
+
+            $.ajax({
+              type: "POST",
+              headers: headers,
+              url: "https://apis.openapi.sk.com/tmap/routes?version=1&format=json", //
+              async: false,
+              contentType: "application/json",
+              data: JSON.stringify({
+                reqCoordType: "WGS84GEO",
+                resCoordType: "WGS84GEO",
+                startName: "출발",
+                startX: String(marker_s.getPosition()._lng),
+                startY: String(marker_s.getPosition()._lat),
+                startTime: "202111121314",
+                passList: passList,
+                endName: "도착",
+                endX: String(marker_e.getPosition()._lng),
+                endY: String(marker_e.getPosition()._lat),
+                searchOption: "0",
+              }),
+
+              success: function (response) {
+                var ptrcl = response;
+                drawData(ptrcl, map);
+
+              },
+              error: function (request, status, error) {
+                console.log(
+                  "code:" +
+                    request.status +
+                    "\n" +
+                    "message:" +
+                    request.responseText +
+                    "\n" +
+                    "error:" +
+                    error
+                );
+              },
+            }); //경로 그리기
+            */
+          }
+        }); // ajax
+
+      }); // btn_select
+
+      $("#btn_result").click(function (key) {
+        alert("안전경로 공유 버튼 클릭")
       });
-    }, //useEffect
-    []
-  );
+
+    }, []); //useEffect
 
   return (
     <div className="Map">
+
       <div className="sidebar">
         <input
           type="text"
@@ -375,7 +428,10 @@ const Map = () => {
           name="startPoint"
           placeholder="출발지를 입력해주세요."
         ></input>
+        <button id="btn_start">출발지 입력</button>
+
         <div className="sidebarEnd"></div>
+
         <input
           type="text"
           className="endPoint"
@@ -383,6 +439,8 @@ const Map = () => {
           name="endPoint"
           placeholder="도착지를 입력해주세요."
         ></input>
+
+        <button id="btn_end">도착지 입력</button>
         <button id="btn_select">시작하기</button>
       </div>
 
@@ -394,12 +452,17 @@ const Map = () => {
               <p>
                 안전경로를 추천해줍니다:)
                 <br />
-                우선 출발지와 도착지를 입력해주세요!
+                우선 출발지, 도착지를 입력하고 지도에서 선택해주세요!
               </p>
             </ul>
           </div>
         </div>
       </div>
+
+      <div className="result">
+        <button id="btn_result">안전경로 공유하기</button>
+      </div>
+
       <div
         id="myMap"
         style={{
@@ -411,6 +474,7 @@ const Map = () => {
           textAlign: "right",
         }}
       ></div>
+
     </div>
   );
 };
--- src/Components/OAuth.js
+++
@@ -1,9 +0,0 @@
-const REST_API_KEY = "68b9345cbe4eaf4b204cc04193c2f055";
-const KAKAO_REDIRECT_URL = "http://localhost:3000/oauth";
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=
-${REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URL}&response_type=code`;
-
-const NAVER_CLIENT_ID = "6sA3tt42khJiE9H3xWmE";
-const NAVER_CALLBACK_URL = "http://localhost:3000/oauth";
-export const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type
-=code&client_id=${NAVER_CLIENT_ID}&state=STATE_STRING&redirect_uri=${NAVER_CALLBACK_URL}`;
